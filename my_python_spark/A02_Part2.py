a182f9a46ecbdbd25e15f0885d8a0a094913a4d4
Mateusz <15845263+maateo@users.noreply.github.com>
2020-04-13 16:42:12 +0100

    Adding project files

---
+++ my_python_spark/A02_Part2.py
@@ -0,0 +1,91 @@
+# --------------------------------------------------------
+#
+# PYTHON PROGRAM DEFINITION
+#
+# The knowledge a computer has of Python can be specified in 3 levels:
+# (1) Prelude knowledge --> The computer has it by default.
+# (2) Borrowed knowledge --> The computer gets this knowledge from 3rd party libraries defined by others
+#                            (but imported by us in this program).
+# (3) Generated knowledge --> The computer gets this knowledge from the new functions defined by us in this program.
+#
+# When launching in a terminal the command:
+# user:~$ python3 this_file.py
+# our computer first processes this PYTHON PROGRAM DEFINITION section of the file.
+# On it, our computer enhances its Python knowledge from levels (2) and (3) with the imports and new functions
+# defined in the program. However, it still does not execute anything.
+#
+# --------------------------------------------------------
+
+# ------------------------------------------
+# IMPORTS
+# ------------------------------------------
+import pyspark
+
+import calendar
+import datetime
+
+# ------------------------------------------
+# FUNCTION process_line
+# ------------------------------------------
+def process_line(line):
+    # 1. We create the output variable
+    res = ()
+
+    # 2. We remove the end of line character
+    line = line.replace("\n", "")
+
+    # 3. We split the line by tabulator characters
+    params = line.split(";")
+
+    # 4. We assign res
+    if (len(params) == 7):
+        res = tuple(params)
+
+    # 5. We return res
+    return res
+
+# ------------------------------------------
+# FUNCTION my_main
+# ------------------------------------------
+def my_main(sc, my_dataset_dir, station_name):
+    pass
+
+# --------------------------------------------------------
+#
+# PYTHON PROGRAM EXECUTION
+#
+# Once our computer has finished processing the PYTHON PROGRAM DEFINITION section its knowledge is set.
+# Now its time to apply this knowledge.
+#
+# When launching in a terminal the command:
+# user:~$ python3 this_file.py
+# our computer finally processes this PYTHON PROGRAM EXECUTION section, which:
+# (i) Specifies the function F to be executed.
+# (ii) Define any input parameter such this function F has to be called with.
+#
+# --------------------------------------------------------
+if __name__ == '__main__':
+    # 1. We use as many input arguments as needed
+    station_name = "Fitzgerald's Park"
+
+    # 2. Local or Databricks
+    local_False_databricks_True = False
+
+    # 3. We set the path to my_dataset
+    my_local_path = "/home/nacho/CIT/Tools/MyCode/Spark/"
+    my_databricks_path = "/"
+
+    my_dataset_dir = "FileStore/tables/3_Assignment/my_dataset/"
+
+    if local_False_databricks_True == False:
+        my_dataset_dir = my_local_path + my_dataset_dir
+    else:
+        my_dataset_dir = my_databricks_path + my_dataset_dir
+
+    # 4. We configure the Spark Context
+    sc = pyspark.SparkContext.getOrCreate()
+    sc.setLogLevel('WARN')
+    print("\n\n\n")
+
+    # 5. We call to our main function
+    my_main(sc, my_dataset_dir, station_name)
---
+++ my_python_spark/A02_Part5.py
@@ -0,0 +1,24 @@
+
+# ------------------------------------------
+# FUNCTION my_map
+# ------------------------------------------
+def my_map(my_input_stream, my_output_stream, my_mapper_input_parameters):
+    pass
+
+# ------------------------------------------
+# FUNCTION my_reduce
+# ------------------------------------------
+def my_reduce(my_input_stream, my_output_stream, my_reducer_input_parameters):
+    pass
+
+# ------------------------------------------
+# FUNCTION my_spark_core_model
+# ------------------------------------------
+def my_spark_core_model(sc, my_dataset_dir):
+    pass
+
+# ------------------------------------------
+# FUNCTION my_spark_streaming_model
+# ------------------------------------------
+def my_spark_streaming_model(ssc, monitoring_dir):
+    pass
